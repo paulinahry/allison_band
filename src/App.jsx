906b18edf771f97b43c575dab682d580a4921b7f
Paulina Hryszko <paulina.hryszko@weareslim.de>
2023-06-05 16:15:38 +0200

    basic design hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Home from './pages/Home/Home'
 
 function App() {
-    const [count, setCount] = useState(0)
-
     return (
         <>
-            <p
-                className=" text-red-500 font-bold
-                        text-red-500 font-bold
-                        text-red-500 font-bold"
-            >
-                hallo
-            </p>
+            <Home />
         </>
     )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import heroPic from '/public/images/pexels-brett-sayles-2479312.jpg'
+
+function Hero() {
+    return (
+        <div
+            className=" h-screen bg-cover bg-center 
+            flex flex-col justify-end items-end"
+            style={heroPic ? { backgroundImage: `url(${heroPic})` } : {}}
+        >
+            <div
+                className=" 
+                text-red-800
+                 font-semibold tracking-wider 
+                flex flex-col  justify-self-end 
+                m-4"
+            >
+                <p className="text-7xl">allison</p>
+                <p className="text-5xl">Mermaid calling</p>
+                <p className="text-xl tracking-tight">new album out now</p>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
