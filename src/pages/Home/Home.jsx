7f784b60e882eb16bc9f917d617d6ebb0c5bd5a9
Paulina <98748206+paulinahry@users.noreply.github.com>
2023-06-05 19:21:40 +0200

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,20 +1,15 @@
 import React from 'react'
 import Navigation from '../../components/Navigation'
 import Hero from '../../components/Hero'
-<<<<<<< HEAD
 import Tours from '../Tour/Tour'
-=======
->>>>>>> main
+
 
 function Home() {
     return (
         <div>
             <Navigation />
             <Hero />
-<<<<<<< HEAD
-            <Tours />
-=======
->>>>>>> main
+            <Tour />
         </div>
     )
 }
