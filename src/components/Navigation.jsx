fc0d5d868de7ae69238d317154328ae5abe41632
Paulina Hryszko <paulinahryszko@MacBookPro-Paulina.local>
2023-06-05 20:41:17 +0200

    nav base

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,33 +1,44 @@
-import React from 'react'
-import { useState } from 'react'
+import React from 'react';
+import { useState } from 'react';
 // ICONS
-import { RxHamburgerMenu } from 'react-icons/rx'
-import { AiOutlineClose } from 'react-icons/ai'
+import { RxHamburgerMenu } from 'react-icons/rx';
+import { AiOutlineClose } from 'react-icons/ai';
 
 function Navigation() {
-    const [updateMenu, setUpdateMenu] = useState(false)
-    const [menu, setMenu] = useState(['tour', 'about', 'store'])
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [menuItems] = useState(['tour', 'about', 'store']);
 
-    const toggleMenu = () => {
-        setUpdateMenu(!updateMenu)
-    }
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-    return (
-        <nav
-            className="
-        w-full h-20 md:h-[100]
-        fixed flex justify-between items-center
-         text-red-800 bg-black"
-        >
-            <RxHamburgerMenu
-                className="cursor-pointer"
-                size={45}
-                onClick={toggleMenu}
-            />
+  return (
+    <nav className="w-full h-20 md:h-[100] fixed flex justify-between items-center text-red-800 bg-black">
+      {isMenuOpen ? (
+        <AiOutlineClose
+          className="cursor-pointer"
+          size={45}
+          onClick={toggleMenu}
+        />
+      ) : (
+        <RxHamburgerMenu
+          className="cursor-pointer"
+          size={45}
+          onClick={toggleMenu}
+        />
+      )}
 
-            <div></div>
-        </nav>
-    )
+      {isMenuOpen && (
+        <div className="flex">
+          {menuItems.map((item, index) => (
+            <span key={index} className="px-4 py-2">
+              {item}
+            </span>
+          ))}
+        </div>
+      )}
+    </nav>
+  );
 }
 
-export default Navigation
+export default Navigation;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navigation from '../../components/Navigation'
 import Hero from '../../components/Hero'
-import Tours from '../Tour/Tour'
+import Tour from '../Tour/Tour'
 
 
 function Home() {
