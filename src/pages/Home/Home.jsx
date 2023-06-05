9b872d0ca7bfc40d8ddb904ccfa537fd9295894d
Paulina Hryszko <paulina.hryszko@weareslim.de>
2023-06-05 16:15:38 +0200

    hero basics

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navigation from '../../components/Navigation'
+import Hero from '../../components/Hero'
+
+function Home() {
+    return (
+        <div>
+            <Navigation />
+            <Hero />
+        </div>
+    )
+}
+
+export default Home
