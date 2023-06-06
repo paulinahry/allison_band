0d3b24d5745dcab740ba75e8fc08b8a6349041c5
Paulina Hryszko <paulinahryszko@MacBookPro-Paulina.local>
2023-06-06 22:54:14 +0200

    handleLinkClick function

--- src/pages/Music.jsx
+++ src/pages/Music.jsx
@@ -14,16 +14,35 @@ const divStyle = {
     height: '50px',
     marginRight: '4px',
     textAlign: 'center',
+    cursor:'pointer'
 }
 
-const Wrapper = ({ children }) => {
-    return <div style={divStyle}>{children}</div>
+const Wrapper = ({ children , onClick}) => {
+    return   (
+    <div style={divStyle} onClick={onClick}>
+    {children}
+  </div>)
 }
 
 function Music() {
     const bgImage = {
         backgroundImage: `url(${heroPic})`,
     }
+
+    const handleLinkClick = (text) => {
+        if (text === 'spotify') {
+          window.location.href = 'https://spotify.com';
+        } else if (text === 'tidal') {
+          window.location.href = 'https://tidal.com';
+        } else if (text === 'apple') {
+          window.location.href = 'https://apple.com';
+        } else if (text === 'soundcloud') {
+          window.location.href = 'https://soundcloud.com';
+        } else if (text === 'deezer') {
+          window.location.href = 'https://deezer.com';
+        }
+      }
+
     return (
         <>
             <div
@@ -32,7 +51,7 @@ function Music() {
                 style={bgImage}
             ></div>
             {/* MEDIA */}
-            <div className="w-[40%] relative bottom-[900px] left-[30%]  text-center">
+            <div className="w-[40%] relative bottom-[700px] left-[30%]  text-center">
                 <img src={heroPic} alt="allison band" />
                 <div className="bg-black">
                     <h1 className=" text-2xl sm:text-3xl p-1 ">
@@ -41,24 +60,24 @@ function Music() {
                     <p>choose your music player</p>
                 </div>
                 <div className="bg-white text-black">
-                    <Wrapper>
-                        <SiApplemusic size={30} />
-                        <p> apple music</p>
+                    <Wrapper onClick={() => handleLinkClick('apple')}>
+                        <SiApplemusic size={30} className='mr-4'/>
+                        <p> apple</p>
                     </Wrapper>
-                    <Wrapper>
-                        <BsSpotify size={30} />
+                    <Wrapper onClick={() => handleLinkClick('spotify')}>
+                        <BsSpotify size={30} className='mr-4'/>
                         <p>spotify</p>
                     </Wrapper>
-                    <Wrapper>
-                        <SiTidal size={30} />
+                    <Wrapper onClick={() => handleLinkClick('tidal')}>
+                        <SiTidal size={30} className='mr-6'/>
                         <p>tidal</p>
                     </Wrapper>
-                    <Wrapper>
-                        <ImSoundcloud size={30} />
+                    <Wrapper onClick={() => handleLinkClick('soundcloud')}>
+                        <ImSoundcloud size={30} className='mr-4'/>
                         <p>soundcloud</p>
                     </Wrapper>
-                    <Wrapper>
-                        <FaDeezer size={30} />
+                    <Wrapper onClick={() => handleLinkClick('deezer')}>
+                        <FaDeezer size={30} className='mr-4'/>
                         <p>deezer</p>
                     </Wrapper>
                 </div>
