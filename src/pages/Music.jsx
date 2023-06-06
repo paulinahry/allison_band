8f9993f5ad3f4811e0f6a0695e25812652a5d0b1
Paulina Hryszko <paulinahryszko@MacBookPro-Paulina.local>
2023-06-06 22:59:00 +0200

    function handleLinkClick optimized

--- src/pages/Music.jsx
+++ src/pages/Music.jsx
@@ -1,89 +1,70 @@
-import React from 'react'
-import heroPic from '/src/assets/images/pexels-brett-sayles-2479312.jpg'
-import { BsSpotify } from 'react-icons/bs'
-import { SiTidal } from 'react-icons/si'
-import { ImSoundcloud } from 'react-icons/im'
-import { SiApplemusic } from 'react-icons/si'
-import { FaDeezer } from 'react-icons/fa'
+import React from 'react';
+import heroPic from '/src/assets/images/pexels-brett-sayles-2479312.jpg';
+import { BsSpotify } from 'react-icons/bs';
+import { SiTidal } from 'react-icons/si';
+import { ImSoundcloud } from 'react-icons/im';
+import { SiApplemusic } from 'react-icons/si';
+import { FaDeezer } from 'react-icons/fa';
 
 const divStyle = {
-    borderBottom: '0.5px solid black',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '50px',
-    marginRight: '4px',
-    textAlign: 'center',
-    cursor:'pointer'
-}
+  borderBottom: '0.5px solid black',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '50px',
+  marginRight: '4px',
+  textAlign: 'center',
+  cursor: 'pointer',
+};
 
-const Wrapper = ({ children , onClick}) => {
-    return   (
+const Wrapper = ({ children, onClick }) => {
+  return (
     <div style={divStyle} onClick={onClick}>
-    {children}
-  </div>)
-}
+      {children}
+    </div>
+  );
+};
 
 function Music() {
-    const bgImage = {
-        backgroundImage: `url(${heroPic})`,
-    }
+  const bgImage = {
+    backgroundImage: `url(${heroPic})`,
+  };
 
-    const handleLinkClick = (text) => {
-        if (text === 'spotify') {
-          window.location.href = 'https://spotify.com';
-        } else if (text === 'tidal') {
-          window.location.href = 'https://tidal.com';
-        } else if (text === 'apple') {
-          window.location.href = 'https://apple.com';
-        } else if (text === 'soundcloud') {
-          window.location.href = 'https://soundcloud.com';
-        } else if (text === 'deezer') {
-          window.location.href = 'https://deezer.com';
-        }
-      }
+  const handleLinkClick = (url) => {
+    window.location.href = url;
+  };
 
-    return (
-        <>
-            <div
-                className="h-screen w-full bg-cover blur-lg
-    flex flex-col justify-end items-end"
-                style={bgImage}
-            ></div>
-            {/* MEDIA */}
-            <div className="w-[40%] relative bottom-[700px] left-[30%]  text-center">
-                <img src={heroPic} alt="allison band" />
-                <div className="bg-black">
-                    <h1 className=" text-2xl sm:text-3xl p-1 ">
-                        Allison - <span>play new album</span>
-                    </h1>
-                    <p>choose your music player</p>
-                </div>
-                <div className="bg-white text-black">
-                    <Wrapper onClick={() => handleLinkClick('apple')}>
-                        <SiApplemusic size={30} className='mr-4'/>
-                        <p> apple</p>
-                    </Wrapper>
-                    <Wrapper onClick={() => handleLinkClick('spotify')}>
-                        <BsSpotify size={30} className='mr-4'/>
-                        <p>spotify</p>
-                    </Wrapper>
-                    <Wrapper onClick={() => handleLinkClick('tidal')}>
-                        <SiTidal size={30} className='mr-6'/>
-                        <p>tidal</p>
-                    </Wrapper>
-                    <Wrapper onClick={() => handleLinkClick('soundcloud')}>
-                        <ImSoundcloud size={30} className='mr-4'/>
-                        <p>soundcloud</p>
-                    </Wrapper>
-                    <Wrapper onClick={() => handleLinkClick('deezer')}>
-                        <FaDeezer size={30} className='mr-4'/>
-                        <p>deezer</p>
-                    </Wrapper>
-                </div>
-            </div>
-        </>
-    )
+  const musicPlayers = [
+    { name: 'apple', icon: <SiApplemusic size={30} className="mr-4" />, url: 'https://apple.com' },
+    { name: 'spotify', icon: <BsSpotify size={30} className="mr-4" />, url: 'https://spotify.com' },
+    { name: 'tidal', icon: <SiTidal size={30} className="mr-6" />, url: 'https://tidal.com' },
+    { name: 'soundcloud', icon: <ImSoundcloud size={30} className="mr-4" />, url: 'https://soundcloud.com' },
+    { name: 'deezer', icon: <FaDeezer size={30} className="mr-4" />, url: 'https://deezer.com' },
+  ];
+
+  return (
+    <>
+      <div className="h-screen w-full bg-cover blur-lg flex flex-col justify-end items-end" style={bgImage}></div>
+      {/* MEDIA */}
+      <div className="w-[40%] relative bottom-[700px] left-[30%] text-center">
+        <img src={heroPic} alt="allison band" />
+        <div className="bg-black">
+          <h1 className="text-2xl sm:text-3xl p-1">
+            Allison - <span>play new album</span>
+          </h1>
+          <p>choose your music player</p>
+        </div>
+        <div className="bg-white text-black">
+          {musicPlayers.map((player) => (
+            <Wrapper key={player.name} onClick={() => handleLinkClick(player.url)}>
+              {player.icon}
+              <p>{player.name}</p>
+            </Wrapper>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Music
+export default Music;
