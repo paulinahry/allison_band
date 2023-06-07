0f0288eca465086846ec661e6cadcd235c5f115e
Paulina Hryszko <paulina.hryszko@weareslim.de>
2023-06-07 08:44:33 +0200

    scroll effect from the top

--- src/pages/Tour.jsx
+++ src/pages/Tour.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import tour from '../pages/dataTour'
 import CardTour from '../components/CardTour'
+import { useEffect } from 'react'
 
 function Tour() {
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [])
+
     return (
         <div className="flex justify-center align-center text-center">
-            <div className="py-10 text-detailsRed md:w-[80%]">
+            <div className="py-10 text-detailsRed">
                 <h2 className="uppercase font-bold text-3xl ">
                     Mermaid calling tour 2023
                 </h2>
