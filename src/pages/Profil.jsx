5b79847023963747c8c638639ca61df8199fb587
Paulina Hryszko <paulinahryszko@MacBookPro-Paulina.local>
2023-06-12 22:37:59 +0200

    rd

--- src/pages/Profil.jsx
+++ src/pages/Profil.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
 import { useData } from '../context/UseContext'
 import CardOrders from '../components/CardOrders'
 
 const Profil = () => {
     const { userOrders, user } = useData()
+    
 
     if (!user) {
         return <p>User not logged in</p>
@@ -15,6 +15,7 @@ const Profil = () => {
 
     return (
         <div className="profil h-screen">
+   
             {userOrders.map((order) => (
                 <CardOrders key={order._id} orders={order} />
             ))}
