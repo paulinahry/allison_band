2862e894912d76672692237a4359eb8d5fd30a06
Paulina Hryszko <paulina.hryszko@weareslim.de>
2023-06-13 08:38:19 +0200

    hero pic

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,29 @@
 import heroPic from '/src/assets/images/pexels-brett-sayles-2479312.jpg'
 
-
-
+// import {
+//     loadImage,
+//     applyTransforms,
+//     builtins,
+//     generateTransforms,
+// } from 'imagetools-core'
 
 function Hero() {
+    // const [loading, setLoading] = useState(true)
+    // const image = loadImage(heroPic)
+    // const config = {
+    //     width: '100vh',
+    //     height: '100vh',
+    //     format: 'webp',
+    // }
+    // const { transforms } = generateTransforms(config, builtins)
+    // const { image: transformedImage, metadata } = applyTransforms(
+    //     transforms,
+    //     image
+    // )
 
+    // if (loading) {
+    //     return <Spinner />
+    // }
 
     return (
         <div
@@ -23,70 +42,3 @@ function Hero() {
 }
 
 export default Hero
-
-
-
-
-
-
-
-
-
-// import React, { useEffect, useRef, useState } from 'react';
-// import heroPic from '/src/assets/images/pexels-brett-sayles-2479312.jpg';
-// import { loadImage, applyTransforms, builtins, generateTransforms } from 'imagetools-core';
-
-// async function applyImageTransforms(image) {
-//   const config = {
-//     width: '100vh',
-//     height: '100vh',
-//     format: 'webp',
-//   };
-
-//   const { transforms } = generateTransforms(config, builtins);
-//   const { image: transformedImage, metadata } = await applyTransforms(transforms, image);
-
-//   return { transformedImage, metadata };
-// }
-
-// function Hero() {
-//   const [loading, setLoading] = useState(true);
-//   const [error, setError] = useState(false);
-
-//   useEffect(() => {
-//     const loadImageFunc = async () => {
-//       try {
-//         const image = await loadImage(heroPic);
-//         const { transformedImage, metadata } = await applyImageTransforms(image);
-
-//        
-//         setLoading(false);
-//       } catch (error) {
-//         setError(true);
-//         setLoading(false);
-//         console.error('Error loading or transforming image:', error);
-//       }
-//     };
-
-//     loadImageFunc();
-//   }, []);
-
-//   if (loading) {
-//     return <Spinner />;
-//   }
-
-//   
-
-//   return (
-//     <div className="h-screen bg-cover bg-center">
-//       <img  className="h-full w-full" src={heroPic} alt="allison" />
-//       <div className="text-detailsRed font-semibold tracking-wider flex flex-col justify-self-end m-4">
-//         <p className="text-7xl sm:text-9xl">allison</p>
-//         <p className="text-5xl sm:text-7xl">Mermaid calling</p>
-//         <p className="text-3xl sm:text-5xl tracking-tight text-yellow-300">new album out now</p>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Hero;
