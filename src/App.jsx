d0de73f90bc87ebbabc859569a7669527bedfbb5
Paulina Hryszko <paulina.hryszko@weareslim.de>
2023-06-14 11:41:09 +0200

    Implement Redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom'
+import {
+    BrowserRouter as Router,
+    Route,
+    Link,
+    Routes,
+    useNavigate,
+    useLocation,
+} from 'react-router-dom'
 
+import { history } from 'src/helpers'
 import Navigation from 'components/Navigation'
 import Home from './pages/Home'
 import Tour from './pages/Tour'
@@ -9,25 +17,28 @@ import Music from './pages/Music'
 import Footer from 'components/Footer'
 import Login from './pages/Login'
 import Profil from './pages/Profil'
+import Register from './pages/Register'
 
 function App() {
+    history.navigate = useNavigate()
+    history.location = useLocation()
+
     return (
-        <Router>
-            <div id="root">
-                <Navigation />
-                <div className="content pt-20">
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/tour" element={<Tour />} />
-                        <Route path="/store" element={<Store />} />
-                        <Route path="/music" element={<Music />} />
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/profil" element={<Profil />} />
-                    </Routes>
-                </div>
-                <Footer />
+        <div id="root">
+            <Navigation />
+            <div className="content pt-20">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/tour" element={<Tour />} />
+                    <Route path="/store" element={<Store />} />
+                    <Route path="/music" element={<Music />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/profil" element={<Profil />} />
+                    <Route path="/register" element={<Register />} />
+                </Routes>
             </div>
-        </Router>
+            <Footer />
+        </div>
     )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import DataProvider from './context/UseContext.jsx'
+import store from './redux/store'
+import { Provider } from 'react-redux'
+
+import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <DataProvider>
-            <App />
-        </DataProvider>
+        <Provider store={store}>
+            <Router>
+                <App />
+            </Router>
+        </Provider>
     </React.StrictMode>
 )
