241257ea0453ce929b179862943e2db15cf3c692
Paulina Hryszko <paulina.hryszko@weareslim.de>
2023-06-15 12:51:26 +0200

    redux

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,11 +1,11 @@
-import { productContext } from '../context/useProduct'
 import CardProduct from 'components/CardProduct'
+import { useSelector, useDispatch } from 'react-redux'
 
 const Product = () => {
-    const { products } = productContext()
+    const { products } = useSelector((s) => s.prod)
 
     return (
-        <div className="store">
+        <div className="products">
             <h2>Products</h2>
             {products.map((product) => (
                 <CardProduct key={product._id} product={product} />
