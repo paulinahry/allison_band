732c51c9e254e6fdd287593b87087ca417e99850
Paulina Hryszko <paulina.hryszko@weareslim.de>
2023-06-16 10:58:04 +0200

    backend for cart actions

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -30,7 +30,7 @@ const login = async (req, res) => {
         res.status(200).json({ message: 'Login successful', user })
     } catch (error) {
         console.error(error)
-        res.status(500).json({ error: 'An error occurred during login' })
+        res.status(500).send({ error: 'An error occurred during login' })
     }
 }
 const logout = async (req, res) => {
@@ -41,19 +41,85 @@ const register = async (req, res) => {
     const user = new User(req.body)
     res.status(200).send()
 
-    await user.save()
+    await user.save({ message: 'User registered correctly' })
 }
 
+//--------------- user actions in cart ------------------
 
 const getCart = async (req, res) => {
     try {
-      const user = await User.findById(req.user._id).populate('cart.product')
-  
-      res.status(200).send(user.cart)
-    } catch (error) {
-      console.error(error)
-      res.status(500).json({ error: 'An error occurred while fetching the cart' })
-    }
-  }
+        const user = await User.findById(req.user._id)
+            .populate('cart.product')
+            .populate('cart.amount')
 
-export default { getUser, getUserById, login, logout, register, getCart }
+        res.status(200).send(user.cart)
+    } catch (error) {
+        console.error(error)
+        res.status(500).send({
+            error: 'An error occurred while fetching the cart',
+        })
+    }
+}
+
+const addToCart = async (req, res) => {
+    try {
+        const { userId } = req.params
+        const { productId, quantity } = req.body
+
+        const user = await User.findOne({ _id: req.params.id })
+        user.cart.push({ productId, quantity })
+
+        user.save()
+
+        res.status(200).send({ message: 'Product added to cart', user })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            error: 'Internal Server Error: product can`t be added to the cart',
+        })
+    }
+}
+
+const removeOne = async (req, res) => {
+    try {
+        const { productId, userId } = req.body
+
+        const user = await User.findOne({ _id: req.params.id })
+        user.cart.filter((item = item.productId !== productId))
+
+        user.save()
+
+        res.status(200).send({ message: 'Product removed from cart' })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            error: 'Internal Server Error: product is not removed from cart',
+        })
+    }
+}
+
+const removeAll = async (req, res) => {
+    try {
+        const user = await User.findOne({ _id: req.params._id })
+        user.cart = []
+        user.save()
+        res.status(200).send({ message: 'Cart is empty' })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            error: 'Internal Server Error: cannot empty the cart',
+        })
+    }
+}
+
+export default {
+    getUser,
+    getUserById,
+    login,
+    logout,
+    register,
+    getCart,
+    addToCart,
+    removeOne,
+    removeAll,
+}
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -8,5 +8,8 @@ app.get('/logout', userController.logout)
 app.post('/login', userController.login)
 app.post('/register', userController.register)
 app.get('/cart', userController.getCart)
+app.post('/add', userController.addToCart)
+app.delete('/remove', userController.removeOne)
+app.delete('/removeAll', userController.removeAll)
 
 export default app
