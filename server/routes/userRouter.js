5bb495090d9e62a96c9fddc2001947405c35a5ad
Paulina Hryszko <paulina.hryszko@weareslim.de>
2023-06-20 17:25:02 +0200

    auth token middleware before user actions

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,15 +1,17 @@
 import express from 'express'
 import userController from '../controllers/userController.js'
+import authTokenMiddleware from '../middlewares/token.middleware.js'
 const app = express.Router()
 
 app.get('/', userController.getUser)
 app.get('/logout', userController.logout)
 //app.get('/:id', userController.getUserById)
 app.post('/login', userController.login)
+app.post('/login/refresh-token', userController.refresh)
 app.post('/register', userController.register)
-app.post('/cart', userController.getCart)
-app.post('/cart/add', userController.addToCart)
-app.post('/cart/remove', userController.removeOne)
-app.delete('/cart/removeAll', userController.removeAll)
+app.get('/cart', authTokenMiddleware, userController.getCart)
+app.post('/cart/add', authTokenMiddleware, userController.addToCart)
+app.post('/cart/remove', authTokenMiddleware, userController.removeOne)
+app.delete('/cart/removeAll', authTokenMiddleware, userController.removeAll)
 
 export default app
