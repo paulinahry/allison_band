1f4678d90b9ab2e7b45414e25dd38702590f81a3
Paulina Hryszko <paulina.hryszko@weareslim.de>
2023-06-21 09:59:19 +0200

    token auth

--- server/controllers/ordersController.js
+++ server/controllers/ordersController.js
@@ -1,4 +1,13 @@
 import Order from '../models/orderModel.js'
+import jwt from 'jsonwebtoken'
+import User from '../models/userModel.js'
+
+const tokenLifetime = 10 * 10 * 1000 //10 min, than refresh function with new token
+const refreshTokenLifetime = 24 * 60 * 60 * 1000
+
+function generateAccessToken(data, expiresIn) {
+    return jwt.sign(data, process.env.TOKEN_SECRET, { expiresIn })
+}
 
 const getOrders = async (req, res) => {
     const orders = await Order.find().populate('items.product').populate('user')
@@ -9,26 +18,51 @@ const getOrders = async (req, res) => {
 //     const order = await Order.findById(req.params.id).populate('user')
 //     res.status(200).send(order)
 // }
-// 
-// 
+//
+//
 const getUsersOrders = async (req, res) => {
     try {
-      const order = await Order.findById(req.params.id)
-        .populate('user')
-        .populate('items.product')
-  
-      const populatedOrder = {
-        ...order.toObject(),
-        items: order.items.map((item) => ({
-          ...item.toObject(),
-          product: item.product,
-        })),
-      }
-  
-      res.status(200).send(populatedOrder)
+        const { token } = req.cookies
+        let tokenData
+        try {
+            jwt.verify(token, String(process.env.TOKEN_SECRET), (err, data) => {
+                if (err || !data) {
+                    throw new Error(err)
+                }
+                tokenData = data
+            })
+        } catch (error) {
+            return res.sendStatus(401)
+        }
+
+        const user = await User.findOne({ _id: tokenData.id }).populate('order')
+        res.status(200).sebd({
+            order: user.order,
+            messege: 'get user order ',
+        })
     } catch (error) {
-      console.error(error)
-      res.status(500).json({ error: 'An error occurred while fetching the order' })
+        console.log(error)
+        res.status(500).send({
+            error: 'An error occured while fetching user order',
+        })
     }
-  }
+
+    // const order = await Order.findById(req.params.id)
+    //   .populate('user')
+    //   .populate('items.product')
+
+    // const populatedOrder = {
+    //   ...order.toObject(),
+    //   items: order.items.map((item) => ({
+    //     ...item.toObject(),
+    //     product: item.product,
+    //   })),
+    // }
+
+    // res.status(200).send(populatedOrder)
+    // } catch (error) {
+    //   console.error(error)
+    //   res.status(500).json({ error: 'An error occurred while fetching the order' })
+    // }
+}
 export default { getOrders, getUsersOrders }
