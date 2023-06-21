6130bb95d0fd053ac5d4cbfcc1e16c5e79002427
Paulina Hryszko <paulina.hryszko@weareslim.de>
2023-06-21 09:59:41 +0200

    token auth

--- server/controllers/ordersController.js
+++ server/controllers/ordersController.js
@@ -14,12 +14,6 @@ const getOrders = async (req, res) => {
     res.status(200).send(orders)
 }
 
-// const getUsersOrders = async (req, res) => {
-//     const order = await Order.findById(req.params.id).populate('user')
-//     res.status(200).send(order)
-// }
-//
-//
 const getUsersOrders = async (req, res) => {
     try {
         const { token } = req.cookies
@@ -46,23 +40,5 @@ const getUsersOrders = async (req, res) => {
             error: 'An error occured while fetching user order',
         })
     }
-
-    // const order = await Order.findById(req.params.id)
-    //   .populate('user')
-    //   .populate('items.product')
-
-    // const populatedOrder = {
-    //   ...order.toObject(),
-    //   items: order.items.map((item) => ({
-    //     ...item.toObject(),
-    //     product: item.product,
-    //   })),
-    // }
-
-    // res.status(200).send(populatedOrder)
-    // } catch (error) {
-    //   console.error(error)
-    //   res.status(500).json({ error: 'An error occurred while fetching the order' })
-    // }
 }
 export default { getOrders, getUsersOrders }
