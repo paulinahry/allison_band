d1b4921eebfbeae376028858fc55cd689fe9193f
Paulina Hryszko <paulina.hryszko@weareslim.de>
2023-06-22 10:14:12 +0200

    if no data from user, show this component

---
+++ src/components/Info.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import UpcomingEvents from '../components/UpcomingEvents'
+import CardProduct from '../components/CardProduct'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { authActions } from '../redux/slices/auth'
+
+function Info() {
+    const { products } = useSelector((s) => s.prod)
+
+    const submitLogout = () => {
+        dispatch(authActions.logout())
+    }
+
+    return (
+        <div className="h-full bg-white">
+            <div className="uppercase flex  justify-around border-b p-2">
+                <Link className="w-fit" to="/">
+                    Home
+                </Link>
+                <Link className="w-fit" to="/store">
+                    Store
+                </Link>
+                <Link className="w-fit" onClick={submitLogout}>
+                    Logout
+                </Link>
+            </div>
+            <UpcomingEvents />
+            <div>
+                <div className="flex flex-wrap justify-around bg-gray-200 p-5">
+                    {products.map((product) => (
+                        <CardProduct key={product._id} product={product} />
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Info
