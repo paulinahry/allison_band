af752c622502e2ce97c60844f675290a7ba51d5e
Paulina Hryszko <paulina.hryszko@weareslim.de>
2023-06-23 13:50:41 +0200

    mini menu deleted

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -8,44 +8,15 @@ import Spinner from './Spinner'
 
 function Info() {
     const { products } = useSelector((s) => s.prod)
-    const authUser = useSelector((s) => s.auth.user)
     if (!products) return <Spinner />
-
-    const submitLogout = () => {
-        dispatch(authActions.logout())
-    }
-
-    const submitLogin = () => {
-        dispatch(authActions.logout())
-    }
 
     return (
         <div className="h-full bg-white">
-            <div className="uppercase flex  justify-around border-b p-2">
-                <Link className="w-fit" to="/">
-                    Home
-                </Link>
-                <Link className="w-fit" to="/store">
-                    Store
-                </Link>
-
-                {authUser ? (
-                    <Link className="w-fit" onClick={submitLogout}>
-                        Logout
-                    </Link>
-                ) : (
-                    <Link className="w-fit" onClick={submitLogin}>
-                        Log in
-                    </Link>
-                )}
-            </div>
             <UpcomingEvents />
-            <div>
-                <div className="flex flex-wrap justify-around bg-gray-200 p-5">
-                    {products.map((product) => (
-                        <CardProduct key={product._id} product={product} />
-                    ))}
-                </div>
+            <div className="flex flex-wrap justify-around bg-gray-200 p-5">
+                {products.map((product) => (
+                    <CardProduct key={product._id} product={product} />
+                ))}
             </div>
         </div>
     )
