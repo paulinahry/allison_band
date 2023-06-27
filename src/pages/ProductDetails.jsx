1a0331c5482bc755ee9e66af235acbf3241f092b
Paulina Hryszko <paulina.hryszko@weareslim.de>
2023-06-27 13:14:07 +0200

    showing basic details

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import { prodActions } from '../redux/slices/products'
-import CardProduct from '../components/CardProduct'
+import { BiArrowBack } from 'react-icons/bi'
+import ErrorPage from '../pages/ErrorPage'
 
 const ProductDetails = () => {
     const { id } = useParams()
     const dispatch = useDispatch()
+    const navigate = useNavigate()
     const products = useSelector((s) => s.prod.products)
 
     useEffect(() => {
@@ -14,15 +16,33 @@ const ProductDetails = () => {
     }, [])
 
     const myProduct = products.find((prod) => prod.id === products.id)
+    console.log(myProduct)
 
     return (
-        <div className="flex flex-wrap justify-center bg-gray-200 text-main ">
+        <div className="min-h-screen max-h-full flex flex-wrap justify-center bg-gray-200 text-main ">
             {myProduct ? (
-                <div>
-                    <p>{myProduct.title}</p>
+                <div className="max-h-full min-h-screen m-10">
+                    <div>
+                        <img src={myProduct.image} alt={myProduct.title} />
+                    </div>
+                    <p>
+                        {myProduct.title}
+                        {myProduct._id}
+                    </p>
+                    <p>{myProduct.description}</p>
+                    <p>{myProduct.price}</p>
+                    <button>add to cart</button>
+
+                    <button
+                        onClick={() => navigate('/store')}
+                        className="underline block"
+                    >
+                        <BiArrowBack className="inline" />
+                        go back
+                    </button>
                 </div>
             ) : (
-                'nope'
+                <ErrorPage />
             )}
         </div>
     )
