779897e6f645b425ca90b2f7f3d2ac102ad23550
Paulina Hryszko <paulina.hryszko@weareslim.de>
2023-06-27 15:43:28 +0200

    layoput

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { useParams, useNavigate } from 'react-router-dom'
 import { prodActions } from '../redux/slices/products'
+import { cartActions } from '../redux/slices/cart'
 import { BiArrowBack } from 'react-icons/bi'
 import ErrorPage from '../pages/ErrorPage'
 
@@ -15,31 +16,47 @@ const ProductDetails = () => {
         dispatch(prodActions.getProductById({ id }))
     }, [])
 
+    const handleAddToCart = (id) => {
+        dispatch(cartActions.addToCart({ id }))
+    }
+
     const myProduct = products.find((prod) => prod.id === products.id)
-    console.log(myProduct)
 
     return (
-        <div className="min-h-screen max-h-full flex flex-wrap justify-center bg-gray-200 text-main ">
+        <div className="min-h-screen max-h-full  bg-gray-200 text-main ">
+            <button onClick={() => navigate('/store')} className="mt-10">
+                <BiArrowBack className=" inline underline" />
+                go back
+            </button>
             {myProduct ? (
-                <div className="max-h-full min-h-screen m-10">
-                    <div>
+                <div className="max-h-full min-h-screen mx-0 my-auto ">
+                    <div className="max-w-[600px]">
                         <img src={myProduct.image} alt={myProduct.title} />
                     </div>
-                    <p>
-                        {myProduct.title}
-                        {myProduct._id}
-                    </p>
-                    <p>{myProduct.description}</p>
-                    <p>{myProduct.price}</p>
-                    <button>add to cart</button>
-
-                    <button
-                        onClick={() => navigate('/store')}
-                        className="underline block"
-                    >
-                        <BiArrowBack className="inline" />
-                        go back
-                    </button>
+                    <div className="p-5 flex justify-around">
+                        <div>
+                            <p className="extrabold text-xxl">
+                                {myProduct.title}
+                            </p>
+                            <p>{myProduct.description}</p>
+                            <p>{myProduct.price}</p>
+                        </div>
+                        <div className=" ">
+                            <button
+                                className=" 
+                            w-20 h-7
+                            md:w-32 md:h-9 
+                            text-xs md:text-sm
+                            flex justify-center items-center
+                            text-main  bg-details font-bold 
+                            hover:text-details hover:bg-main
+                            cursor-pointer rounded "
+                                onClick={() => handleAddToCart(myProduct._id)}
+                            >
+                                add to cart
+                            </button>
+                        </div>
+                    </div>
                 </div>
             ) : (
                 <ErrorPage />
