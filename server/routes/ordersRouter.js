71a4b003b8ba859cea957be665e25dabb8d5a6f4
Paulina Hryszko <paulina.hryszko@weareslim.de>
2023-06-27 15:07:56 +0200

    routes

--- server/routes/ordersRouter.js
+++ server/routes/ordersRouter.js
@@ -5,7 +5,6 @@ import authTokenMiddleware from '../middlewares/token.middleware.js'
 const app = express.Router()
 
 app.get('/', authTokenMiddleware, ordersController.getOrders)
-app.get('/', authTokenMiddleware, ordersController.addOrder)
 // app.get('/:id', authTokenMiddleware, ordersController.getUsersOrders)
 
 export default app
