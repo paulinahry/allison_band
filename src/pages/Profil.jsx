67bf36e5346c7eb0816113794d8ebcf329093172
Paulina Hryszko <paulina.hryszko@weareslim.de>
2023-06-29 16:10:17 +0200

    layout

--- src/pages/Profil.jsx
+++ src/pages/Profil.jsx
@@ -11,7 +11,6 @@ const Profil = () => {
     const authUser = useSelector((s) => s.auth.user)
     const { orders } = useSelector((s) => s.ord)
     const products = useSelector((s) => s.prod.products)
-    console.log(products)
 
     const dispatch = useDispatch()
     const navigate = useNavigate()
@@ -21,7 +20,7 @@ const Profil = () => {
         dispatch(prodActions.getProducts())
         dispatch(orderActions.getOrders())
     }, [])
-
+    console.log('fff64f5r64fe654', orders)
     const calculateTotalPrice = () => {
         let totalPrice = 0
 
@@ -51,13 +50,35 @@ const Profil = () => {
         navigate('/')
         return null
     }
+    const toPay = () => {
+        let total = 0
+
+        orders.forEach((orderItem) => {
+            const product = products.find((prod) => prod._id === orderItem._id)
+            if (product && product.price) {
+                total += orderItem.amount * product.price
+            }
+        })
+        return total.toFixed(2)
+    }
+
+    const getSubtotalOfProduct = (amount, price) => {
+        return (amount * price).toFixed(2)
+    }
+
+    const getTotalofProducts = () => {
+        return orders.reduce(
+            (total, itemInOrder) => total + itemInOrder.amount,
+            0
+        )
+    }
 
     return (
-        <div className="profil min-h-screen max-h-full bg-gray-200 text-main ">
+        <div className="profil min-h-screen max-h-full bg-white text-main ">
             <p className="bg-white text-center p-5">Welcome {authUser.email}</p>
             {orders.length > 0 && (
                 <p className="bg-white text-center pb-1  text-xl">
-                    Actual orders: {orders.length}
+                    Actuall orders: {orders.length}
                 </p>
             )}
 
@@ -75,9 +96,7 @@ const Profil = () => {
                         const itemInOrder = orders.find(
                             (ord) => ord._id === order._id
                         )
-                        console.log('this is order', itemInOrder)
-                        console.log('items ', itemInOrder.items)
-                        console.log(products)
+
                         const orderedProducts = itemInOrder.items.map((id) => {
                             id = id.product
                             const product = products.filter(
@@ -87,11 +106,44 @@ const Profil = () => {
                         })
 
                         return (
-                            <ul key={order._id}>
+                            <div key={order._id} className="border p-20">
+                                <p className="p-2 bg-details  text-main">
+                                    order ID: {order._id}
+                                </p>
                                 {orderedProducts.map((product) => (
-                                    <li key={product._id}>{product.title}</li>
+                                    <div className="flex justify-between">
+                                        <img
+                                            className="
+                                         h-20 w-20
+                                         sm:h-40 sm:w-40"
+                                            src={product.image}
+                                            alt={product.title}
+                                        />
+                                        <div
+                                            className=" text-sm
+                                     flex flex-col pl-2 justify-around"
+                                        >
+                                            <span className="font-extrabold">
+                                                {product.title}
+                                            </span>
+                                            <span>{product.description}</span>
+                                            <span>${product.price}</span>
+                                        </div>
+
+                                        {orders.length > 0 && (
+                                            <div className="flex flex-col">
+                                                <span>
+                                                    Total products:{' '}
+                                                    {getTotalofProducts()}
+                                                </span>
+                                                <span className="font-bold text-greenish">
+                                                    $ {toPay()}
+                                                </span>
+                                            </div>
+                                        )}
+                                    </div>
                                 ))}
-                            </ul>
+                            </div>
                         )
                     })
                 )}
